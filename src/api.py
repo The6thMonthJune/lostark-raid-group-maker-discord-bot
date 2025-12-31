108be4aecd60a2a138f94870d4c9727f3960ce3d
The6thMonthJune <61687735+The6thMonthJune@users.noreply.github.com>
2026-01-01 03:41:45 +0900

    project founding

---
+++ src/api.py
@@ -0,0 +1,18 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class LostArkAPI:
+    BASE_URL = "https://developer.lostark.game.onstove.com"
+    HEADERS = {
+        'accept' : 'application/json',
+        'authorization' : f"bearer {os.getenv('LOSTARK_API_KEY')}"
+    }
+
+    @classmethod
+    def get_siblings(cls, character_name):
+        url = f"{cls.BASE_URL}/characters/{character_name}/siblings/"
+        response = requests.get(url, headers=cls.HEADERS)
+        return response.json() if response.status_code == 200 else None
